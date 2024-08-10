02a794a892564b431fdc9e92c289367be14e1a18
SETHUPATHY1 <159312508+SETHUPATHY1@users.noreply.github.com>
2024-08-10 12:25:41 -0700

    simple avg program

---
+++ 1.avg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter a number:");
+    scanf("%i",&a);
+    printf("enter a number:");
+    scanf("%i",&b);
+    c=a+b;
+    c=c/2;
+    printf("A avg is:%i",c);
+}
+
